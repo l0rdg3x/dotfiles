0d3628140b4eb7fd10c1e693c312dcac2a25c3d7
l0rdg3x <l0rdg3x@gmail.com>
2022-06-04 22:53:59 +0200

    Add some things to .zshrc

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#Require: git vim tmux curl
+#Require: git vim tmux curl gpg neofetch
 
 #tmux
 ln -s tmux.conf ~/.tmux.conf
@@ -11,3 +11,12 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 #ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#little add to .zshrc
+cat << EOT >> ~/.zshrc
+export GPG_TTY="$(tty)"
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+export PATH="$HOME/bin:$PATH"
+gpgconf --launch gpg-agent
+neofetch
+EOT
