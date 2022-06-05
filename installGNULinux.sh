7dc25f0349df6f6583d6f3cedda552adb17e79d7
l0rdg3x <l0rdg3x@gmail.com>
2022-06-05 18:39:31 +0200

    Fix fix fix

--- installGNULinux.sh
+++ installGNULinux.sh
@@ -4,7 +4,7 @@
 #TODO: Install packages
 
 #tmux
-ln -s tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
 #vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
@@ -12,8 +12,9 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 #ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sed 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
-sed 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc
+sed -i '' -e 's/# export PATH=$HOME\/bin:\/usr\/local\/bin:$PATH/export PATH=$HOME\/bin:\/usr\/local\/bin:$PATH/g' ~/.zshrc > /dev/null
+sed -i '' -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc > /dev/null
+sed -i '' -e 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc > /dev/null
 
 #little add to .zshrc
 cat << EOT >> ~/.zshrc
